5717c960133e3b779ab587ab23dd16869fe1a5f8
ZahwaAnritia <anritia15@gmail.com>
2024-03-04 12:37:48 +0700

    Add project files.

---
+++ MenentukanKelulusan1/MenentukanKelulusan1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	//siswa mempunyai 2 nilai yaitu
+
+	float nMat, nfisika, rerata;
+	string status;
+
+	cout << "Masukkan nilai matematika";
+	cin >> nMat;
+	cout << "Masukkan nilai Fisika";
+	cin >> nfisika;
+
+	rerata = (nfisika + nMat) / 2;
+
+	if (rerata >= 60) {
+		status = "Lulus jalur rerata";
+	}
+	else if (nMat >= 70) {
+		status = "Lulus jalur matematika";
+	}
+	else
+	{
+		status = "Tidak Lulus";
+	}
+	cout << "Statusnya adalah" << status << endl;
+
+	system("pause");
+
+	return 0;
+}
---
+++ PertemuanKedua2/PertemuanKedua2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	srand(time(0));
+
+	int nBilA, nBilB;
+	string status;
+
+
+	nBilA = rand() % 11;
+	nBilB = rand() % 11;
+
+	if (nBilA > nBilB) {
+		status = "Bilangan A lebih besar bilangan B";
+	}
+	else if (nBilA < nBilB) {
+		status = "Bilangan A lebih kecil dari bilangan B";
+
+	}
+	else
+	{
+		status = "Bilangan A lebih kecil dari bilangan B";
+
+	}
+
+	cout << "Bilangan A =" << nBilA << nBilA << endl;
+	cout << "Bilangan B =" << nBilB << endl;
+	cout << "Status bilangan = " << status << endl;
+
+}
